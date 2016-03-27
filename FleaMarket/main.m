2d5d3153da3c7968449fa000148cbbc50e35efc1
BJTU-HT <bjtu-ht@163.com>
2016-03-27 20:01:46 +0800

    在main.m中增加bmob设置

--- FleaMarket/main.m
+++ FleaMarket/main.m
@@ -6,11 +6,15 @@
 //  Copyright © 2016年 H-T. All rights reserved.
 //
 
+#import <BmobSDK/Bmob.h>
 #import <UIKit/UIKit.h>
 #import "AppDelegate.h"
 
 int main(int argc, char * argv[]) {
     @autoreleasepool {
+        NSString *appKey = @"22b77d5878214e8e2bc388d056b58254";
+        [Bmob registerWithAppKey:appKey];
+        
         return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
     }
 }
